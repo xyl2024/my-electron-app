e34bcebcbe77241105fa95d9ffcb8c8249f0c694
xyl <alonely2021@qq.com>
2025-06-07 18:11:50 +0800

    🔥 添加日志、CSP、开发者工具

--- main.js
+++ main.js
@@ -1,42 +1,25 @@
-// CommonJS语法：require(...) 是Node中加载模块的方式，返回模块的导出对象
-// {app, BrowserWindow} 从 require(...) 返回的对象中按键名(id)取值
-// app 是 electron 的模块，控制程序的事件生命周期
-// BrowserWindow 模块创建和管理app的窗口
 const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('node:path')
+const log = require('electron-log')
+log.transports.file.level = 'debug'
 
-// () => {} 箭头函数
-// 这里是将这个箭头函数赋值给createWindow变量了，可以通过createWindow()调用
 const createWindow = () => {
+    log.info('Creating BrowserWindow...')
     const win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js') // 将预加载脚本附加到渲染进程
+            preload: path.join(__dirname, 'preload.js')
         },
     })
 
+    win.webContents.openDevTools() // 打开渲染器 DevTools
     win.loadFile('index.html')
+        .then(() => log.info('load index.html done'))
+        .catch(err => log.error('load index.html failed: ', err))
 }
 
 app.whenReady().then(() => {
     ipcMain.handle('ping', () => 'pong')
-    createWindow() // 应用中的每个页面都在一个单独的进程中运行，我们称这些进程为 渲染器 (renderer) 
-
-    // .on(event, listener) 是Node风格的事件注册
-    // 这段代码仅针对macos
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
-})
-
-// .on(event, listener) 是Node风格的事件注册
-// Windows和Linux上通常希望所有的窗口关闭时退出，mac则不是
-app.on('window-all-closed', () => {
-    // nodejs的process.platform变量检查了当前的平台
-    if (process.platform !== 'darwin') { // 判断是否为mac
-        app.quit()
-    }
+    createWindow()
 })
--- preload.js
+++ preload.js
@@ -5,5 +5,4 @@ contextBridge.exposeInMainWorld('versions', {
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron,
     ping: () => ipcRenderer.invoke('ping'),
-    // 除函数之外，我们也可以暴露变量
 })
